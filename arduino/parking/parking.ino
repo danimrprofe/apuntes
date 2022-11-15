da24d62d31fdad01dbeaba92fd8538ab98e50770
Daniel Moreno <danimrprofe@gmail.com>
2022-11-15 14:02:11 +0100

    Update parking.ino

--- arduino/parking/parking.ino
+++ arduino/parking/parking.ino
@@ -13,6 +13,15 @@ int pinP3Green = 7;
 int P1SensorValue = 0;
 int P2SensorValue = 0;
 int P3SensorValue = 0;
+
+int UmbralLuzP1 = 60;
+int UmbralLuzP2 = 40;
+int UmbralLuzP3 = 100;
+
+boolean P1_ocupado;
+boolean P2_ocupado;
+boolean P3_ocupado;
+
 int pos = 0;    // variable to store the servo position
 Servo myservo;  // create servo object to control a servo
 
@@ -30,6 +39,8 @@ void setup() {
 // loop part
 
 void loop() {
+
+  delay(200);
     
   P1SensorValue = analogRead (pinP1Sensor);
   P2SensorValue = analogRead (pinP2Sensor);
@@ -39,32 +50,38 @@ void loop() {
   Serial.println(P2SensorValue);
   Serial.println(P3SensorValue);
 
-  if (P1SensorValue > 60){
+  if (P1SensorValue > UmbralLuzP1){
     digitalWrite(pinP1Red, LOW);
     digitalWrite(pinP1Green, HIGH);
+    P1_ocupado = false;
   } 
   else{
     digitalWrite(pinP1Red, HIGH);
     digitalWrite(pinP1Green, LOW);
+    P1_ocupado = true;
   }
-    if (P2SensorValue > 60){
-    digitalWrite(pinP2Red, HIGH);
-    digitalWrite(pinP2Green, LOW);
-  }  
-    else{
+    if (P2SensorValue > UmbralLuzP2){
     digitalWrite(pinP2Red, LOW);
     digitalWrite(pinP2Green, HIGH);
+    P2_ocupado = false;
+  }  
+    else{
+    digitalWrite(pinP2Red, HIGH);
+    digitalWrite(pinP2Green, LOW);
+    P2_ocupado = true;
   }
-    if (P3SensorValue > 60){
+    if (P3SensorValue > UmbralLuzP3){
     digitalWrite(pinP3Red, LOW);
     digitalWrite(pinP3Green, HIGH);
+    P3_ocupado = false;
   }
     else{
     digitalWrite(pinP3Red, HIGH);
     digitalWrite(pinP3Green, LOW);
+    P3_ocupado = true;
   }  
-  /*
-  if (P1SensorValue > 60 && P2SensorValue > 60 && P3SensorValue > 60){
+  
+  if (P1_ocupado && P2_ocupado && P3_ocupado){
 
       myservo.write(0);              // tell servo to go to position in variable 'pos
     }
@@ -73,7 +90,7 @@ void loop() {
 
       myservo.write(90);              // tell servo to go to position in variable 'pos'
     }
-  */
+
   
 
 

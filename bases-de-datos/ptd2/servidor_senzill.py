cb4a179baad5fddab1b3f44360cde7a763b07e9b
danimrprofe <danimrprofe@gmail.com>
2025-02-07 12:06:12 +0100

    fdfd

---
+++ bases-de-datos/ptd2/servidor_senzill.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hola')
+def hola():
+    return 'Hola món!'
+
+@app.route('/adeu')
+def adeu():
+    return 'Adeu món!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
